38becfc3bd772a14bd7c16fbe42e82db2ab11ba1
Paul-Henri <paul-henri@Paul-Henris-MacBook-Pro.local>
2024-09-11 23:48:23 +0200

    switched to g2p

--- dataspeech/cpu_enrichments/rate.py
+++ dataspeech/cpu_enrichments/rate.py
@@ -1,31 +1,40 @@
-from phonemizer import phonemize
-from phonemizer.backend import EspeakBackend
+from g2p import make_g2p
 
-backend = EspeakBackend('fr-fr', with_stress=True)
+transducer = make_g2p('fra', 'fra-ipa')
 
 def rate_apply(batch, rank=None, audio_column_name="audio", text_column_name="text"):
-    if isinstance(batch[audio_column_name], list):  
+    if isinstance(batch[text_column_name], list):  
         speaking_rates = []
         phonemes_list = []
-        for text, audio in zip(batch[text_column_name], batch[audio_column_name]):
-            phonemes = phonemize(text, language='fr-fr', backend='espeak', with_stress=True)
-            
-            sample_rate = audio["sampling_rate"]
-            audio_length = len(audio["array"].squeeze()) / sample_rate
-            
-            speaking_rate = len(phonemes) / audio_length
+        if "speech_duration" in batch:
+            for text, audio_duration in zip(batch[text_column_name], batch["speech_duration"]):
+                phonemes = transducer(text).output_string
+                audio_duration = audio_duration if audio_duration != 0 else 0.01
+                speaking_rate = len(phonemes) / audio_duration
+                speaking_rates.append(speaking_rate)
+                phonemes_list.append(phonemes)
+        else:
+            for text, audio in zip(batch[text_column_name], batch[audio_column_name]):
+                phonemes = transducer(text).output_string
+                
+                sample_rate = audio["sampling_rate"]
+                audio_length = len(audio["array"].squeeze()) / sample_rate
+                
+                speaking_rate = len(phonemes) / audio_length
 
-            speaking_rates.append(speaking_rate)
-            phonemes_list.append(phonemes)
+                speaking_rates.append(speaking_rate)
+                phonemes_list.append(phonemes)
         
         batch["speaking_rate"] = speaking_rates
         batch["phonemes"] = phonemes_list
     else:
-        phonemes = phonemize(batch[text_column_name], language='fr-fr', backend='espeak', with_stress=True)
-            
-        sample_rate = batch[audio_column_name]["sampling_rate"]
-        audio_length = len(batch[audio_column_name]["array"].squeeze()) / sample_rate
-        
+        phonemes = transducer(batch[text_column_name]).output_string
+        if "speech_duration" in batch:
+            audio_length = batch["speech_duration"] if batch["speech_duration"] != 0 else 0.01
+        else:
+            sample_rate = batch[audio_column_name]["sampling_rate"]
+            audio_length = len(batch[audio_column_name]["array"].squeeze()) / sample_rate
+
         speaking_rate = len(phonemes) / audio_length
         
         batch["speaking_rate"] = speaking_rate
