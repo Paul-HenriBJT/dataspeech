062ce0e7e28c87b4842047e9e095826f6a4f7738
Paul-Henri <paul-henri@Paul-Henris-MacBook-Pro.local>
2024-09-11 22:28:22 +0200

    phonmeize: french

--- dataspeech/cpu_enrichments/rate.py
+++ dataspeech/cpu_enrichments/rate.py
@@ -1,14 +1,14 @@
 from phonemizer import phonemize
 from phonemizer.backend import EspeakBackend
 
-backend = EspeakBackend('es-es', with_stress=True)
+backend = EspeakBackend('fr-fr', with_stress=True)
 
 def rate_apply(batch, rank=None, audio_column_name="audio", text_column_name="text"):
     if isinstance(batch[audio_column_name], list):  
         speaking_rates = []
         phonemes_list = []
         for text, audio in zip(batch[text_column_name], batch[audio_column_name]):
-            phonemes = phonemize(text, language='es-es', backend='espeak', with_stress=True)
+            phonemes = phonemize(text, language='fr-fr', backend='espeak', with_stress=True)
             
             sample_rate = audio["sampling_rate"]
             audio_length = len(audio["array"].squeeze()) / sample_rate
@@ -21,7 +21,7 @@ def rate_apply(batch, rank=None, audio_column_name="audio", text_column_name="te
         batch["speaking_rate"] = speaking_rates
         batch["phonemes"] = phonemes_list
     else:
-        phonemes = phonemize(batch[text_column_name], language='es-es', backend='espeak', with_stress=True)
+        phonemes = phonemize(batch[text_column_name], language='fr-fr', backend='espeak', with_stress=True)
             
         sample_rate = batch[audio_column_name]["sampling_rate"]
         audio_length = len(batch[audio_column_name]["array"].squeeze()) / sample_rate
