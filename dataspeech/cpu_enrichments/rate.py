d93b72f1fca56cc11d7259b974e3effd30042d81
Paul-Henri <paul-henri@Paul-Henris-MacBook-Pro.local>
2024-09-18 08:42:03 +0200

    updated phonemizer

--- dataspeech/cpu_enrichments/rate.py
+++ dataspeech/cpu_enrichments/rate.py
@@ -1,29 +1,34 @@
 from phonemizer import phonemize
+from phonemizer.backend import EspeakBackend
 
-# Initialize the phonemizer backend for German
-backend = 'espeak'
-language = 'de'
+backend = EspeakBackend('es-es', with_stress=True)
 
-def rate_apply(batch, rank=None, text_column_name="text", duration_column_name="speech_duration"):
-    if isinstance(batch[text_column_name], list):  
+def rate_apply(batch, rank=None, audio_column_name="audio", text_column_name="text"):
+    if isinstance(batch[audio_column_name], list):  
         speaking_rates = []
         phonemes_list = []
-        for text, duration in zip(batch[text_column_name], batch[duration_column_name]):
-            phonemes = phonemize(text, language=language, backend=backend, strip=True)
-            duration = max(duration, 0.01)  # Avoid division by zero
-            speaking_rate = len(phonemes) / duration
+        for text, audio in zip(batch[text_column_name], batch[audio_column_name]):
+            phonemes = phonemize(text, language='es-es', backend='espeak', with_stress=True)
+            
+            sample_rate = audio["sampling_rate"]
+            audio_length = len(audio["array"].squeeze()) / sample_rate
+            
+            speaking_rate = len(phonemes) / audio_length
+
             speaking_rates.append(speaking_rate)
             phonemes_list.append(phonemes)
         
         batch["speaking_rate"] = speaking_rates
         batch["phonemes"] = phonemes_list
     else:
-        phonemes = phonemize(batch[text_column_name], language=language, backend=backend, strip=True)
-        duration = max(batch[duration_column_name], 0.01)  # Avoid division by zero
-        speaking_rate = len(phonemes) / duration
+        phonemes = phonemize(batch[text_column_name], language='es-es', backend='espeak', with_stress=True)
+            
+        sample_rate = batch[audio_column_name]["sampling_rate"]
+        audio_length = len(batch[audio_column_name]["array"].squeeze()) / sample_rate
+        
+        speaking_rate = len(phonemes) / audio_length
         
         batch["speaking_rate"] = speaking_rate
         batch["phonemes"] = phonemes
 
     return batch
-
