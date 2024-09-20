1087041b1be74100e512075915aa68c6665ff0bb
Paul-Henri <paul-henri@Paul-Henris-MacBook-Pro.local>
2024-09-20 10:08:32 +0200

    updated model calling

--- scripts/compute_gender_from_pitch.py
+++ scripts/compute_gender_from_pitch.py
@@ -1,40 +1,142 @@
 import argparse
-from datasets import load_dataset
-from transformers import AutoProcessor, AutoModelForAudioClassification
 import torch
+import torchaudio
 import numpy as np
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+from torch.nn import functional as F
+from transformers import AutoFeatureExtractor, AutoModelForAudioClassification, Wav2Vec2Processor
+from tqdm import tqdm
+from typing import List, Optional, Union, Dict
 
-def classify_gender(audio, processor, model):
-    """
-    Classify gender based on audio using a pre-trained model.
-    """
-    inputs = processor(audio, sampling_rate=16000, return_tensors="pt")
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, dataset, audio_column: str, sampling_rate: int = 16000, max_audio_len: int = 5):
+        self.dataset = dataset
+        self.audio_column = audio_column
+        self.sampling_rate = sampling_rate
+        self.max_audio_len = max_audio_len
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        audio = self.dataset[index][self.audio_column]
+        
+        if isinstance(audio, dict):
+            speech_array = torch.tensor(audio['array'])
+            sr = audio['sampling_rate']
+        else:
+            speech_array, sr = torchaudio.load(audio)
+
+        if speech_array.shape[0] > 1:
+            speech_array = torch.mean(speech_array, dim=0, keepdim=True)
+
+        if sr != self.sampling_rate:
+            transform = torchaudio.transforms.Resample(sr, self.sampling_rate)
+            speech_array = transform(speech_array)
+
+        len_audio = speech_array.shape[1]
+
+        if len_audio < self.max_audio_len * self.sampling_rate:
+            padding = torch.zeros(1, self.max_audio_len * self.sampling_rate - len_audio)
+            speech_array = torch.cat([speech_array, padding], dim=1)
+        else:
+            speech_array = speech_array[:, :self.max_audio_len * self.sampling_rate]
+
+        speech_array = speech_array.squeeze().numpy()
+
+        return {"input_values": speech_array, "attention_mask": None}
+
+class CollateFunc:
+    def __init__(self, processor: Wav2Vec2Processor, padding: Union[bool, str] = True,
+                 pad_to_multiple_of: Optional[int] = None, return_attention_mask: bool = True,
+                 sampling_rate: int = 16000, max_length: Optional[int] = None):
+        self.processor = processor
+        self.padding = padding
+        self.pad_to_multiple_of = pad_to_multiple_of
+        self.return_attention_mask = return_attention_mask
+        self.sampling_rate = sampling_rate
+        self.max_length = max_length
+
+    def __call__(self, batch: List[Dict[str, np.ndarray]]):
+        input_values = [item["input_values"] for item in batch]
+
+        batch = self.processor(
+            input_values,
+            sampling_rate=self.sampling_rate,
+            return_tensors="pt",
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_attention_mask=self.return_attention_mask
+        )
+
+        return {
+            "input_values": batch.input_values,
+            "attention_mask": batch.attention_mask if self.return_attention_mask else None
+        }
+
+def predict(dataloader, model, device: torch.device):
+    model.to(device)
+    model.eval()
+    preds = []
+
     with torch.no_grad():
-        logits = model(**inputs).logits
-    predicted_class_id = logits.argmax().item()
-    return model.config.id2label[predicted_class_id]
+        for batch in tqdm(dataloader):
+            input_values, attention_mask = batch['input_values'].to(device), batch['attention_mask'].to(device)
 
-def add_gender_column(example, audio_column, processor, model):
-    """
-    Add a gender column to the dataset based on audio classification.
-    """
-    example['gender'] = classify_gender(example[audio_column], processor, model)
-    return example
+            logits = model(input_values, attention_mask=attention_mask).logits
+            scores = F.softmax(logits, dim=-1)
+
+            pred = torch.argmax(scores, dim=1).cpu().detach().numpy()
+
+            preds.extend(pred)
+
+    return preds
 
 def main(args):
     # Load the dataset
     dataset = load_dataset(args.dataset_name, args.configuration)
 
-    # Initialize the gender classification model and processor
-    processor = AutoProcessor.from_pretrained("alefiury/wav2vec2-large-xlsr-53-gender-recognition-librispeech")
-    model = AutoModelForAudioClassification.from_pretrained("alefiury/wav2vec2-large-xlsr-53-gender-recognition-librispeech")
+    # Model configuration
+    model_name_or_path = "alefiury/wav2vec2-large-xlsr-53-gender-recognition-librispeech"
+    label2id = {"female": 0, "male": 1}
+    id2label = {0: "female", 1: "male"}
+    num_labels = 2
 
-    # Add the gender column
+    # Initialize the feature extractor and model
+    feature_extractor = AutoFeatureExtractor.from_pretrained(model_name_or_path)
+    model = AutoModelForAudioClassification.from_pretrained(
+        pretrained_model_name_or_path=model_name_or_path,
+        num_labels=num_labels,
+        label2id=label2id,
+        id2label=id2label,
+    )
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Process each split in the dataset
     for split in dataset:
-        dataset[split] = dataset[split].map(
-            lambda x: add_gender_column(x, args.audio_column, processor, model),
-            num_proc=args.num_workers
+        custom_dataset = CustomDataset(dataset[split], args.audio_column, max_audio_len=5)
+
+        data_collator = CollateFunc(
+            processor=feature_extractor,
+            padding=True,
+            sampling_rate=16000,
         )
+
+        dataloader = DataLoader(
+            dataset=custom_dataset,
+            batch_size=16,
+            collate_fn=data_collator,
+            shuffle=False,
+            num_workers=2
+        )
+
+        preds = predict(dataloader=dataloader, model=model, device=device)
+
+        # Add predictions to the dataset
+        dataset[split] = dataset[split].add_column("gender_prediction", [id2label[pred] for pred in preds])
 
     # Save the updated dataset
     if args.output_dir:
@@ -55,7 +157,6 @@ if __name__ == "__main__":
     parser.add_argument("--audio_column", type=str, default="audio", help="Name of the column containing audio data for classification.")
     parser.add_argument("--output_dir", type=str, default=None, help="Directory to save the updated dataset.")
     parser.add_argument("--repo_id", type=str, default=None, help="Repository name to push the dataset to the Hugging Face Hub.")
-    parser.add_argument("--num_workers", type=int, default=1, help="Number of worker processes for dataset processing.")
     parser.add_argument("--hub_token", type=str, default=None, help="Hugging Face API token.")
 
     args = parser.parse_args()
