f4a22d72a66adde93e2c96e2e39447fffb2abd90
Paul-HenriBJT <phbiojout@gmail.com>
2024-09-25 16:53:22 +0200

    p

--- scripts/remove_short_audios.py
+++ scripts/remove_short_audios.py
@@ -39,9 +39,9 @@ def filter_and_upload_dataset(dataset_name, new_dataset_name, local_path, subset
 if __name__ == "__main__":
     # Replace these with your actual values
     original_dataset = "ylacombe/cml-tts"
-    new_dataset = "PHBJT/cml-tts"
+    new_dataset = "PHBJT/cml-tts-cleaned"
     local_save_path = "./filtered_dataset"
     dataset_subset = "german"  # Set to None if there's no specific subset
-    min_duration = 1  # Minimum duration in seconds
+    min_duration = 1.5  # Minimum duration in seconds
 
     filter_and_upload_dataset(original_dataset, new_dataset, local_save_path, subset=dataset_subset, min_duration=min_duration)
